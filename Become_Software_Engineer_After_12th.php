f25bfd983521c01c5600683133a1805cfa55b318
pawankgoyal <goyalpawan357@gmail.com>
2022-10-31 14:27:11 +0530

    changes in 12th file

--- Become_Software_Engineer_After_12th.php
+++ Become_Software_Engineer_After_12th.php
@@ -5,17 +5,24 @@
 <div class="course_detail">
     <div class="cr_detail_sec">
 
-        <h3><strong><span itemprop="itemreviewed">Become Software Engineer After 12th in jaipur | Become Programmer/Coder After 12th in jaipur&nbsp;</span></strong></h3>
+        <h3><strong><span itemprop="itemreviewed"> Become Software Engineer After 12th in jaipur | Become Programmer/Coder After 12th in jaipur | Become Software Engineer After 12th &nbsp;</span></strong></h3>
 
         <p><span itemprop="rating" itemscope="" itemtype="http://data-vocabulary.org/Rating"><img alt="4.9 Star Rating: Very Good" src="assets/images/star_rating_groot_Academy.jpg" style="margin-left: 22px;" /> <span itemprop="average">4.9</span> out of <span itemprop="best">5</span> </span>based on <span itemprop="votes">4128</span> Votes.</p>
 
-        <p style="text-align: justify;">Are you Looking Best institute for <strong>React js&nbsp;Training Course in Noida</strong>? DUCAT offers React Training classes with live project by expert trainer in Noida Sector 16A. Our <strong>React JavaScript&nbsp;training program&nbsp;in Noida</strong> is specially designed for Students, Under-Graduates, Graduates, Working Professionals and Freelancers. We provide end to end learning on React Domain with deeper dives for creating a winning career for every profile.</p>
+        <h5>How to become a software engineer after 12th?
+        </h5>
+        <!-- <p style="text-align: justify;"><strong>In this article let’s take a look at how you can become a software engineer after 10th.
+            </strong></p> -->
+        <p style="text-align: justify;">Computer science and Software Engineering have been the charm of the industry since the past
+            few decades and have established themselves as a go-to option amongst the Science stream
+            students throughout 10th and 12th standards.
+            There are several routes one can explore if you want to enter this field early and work your way
+            to the top. Further, many professionals have also gone on to work with multinational companies
+            abroad. In this blog on ‘How to Become a Software Engineer After 12th’, we present to you a
+            guide on starting the profession early on and building a successful career!</p>
+        <!-- <p class="mb-0">So without further ADO let’s get started.</p> -->
 
-        <h4>Why To Enroll In Our React Js Training Course in Noida?</h4>
 
-        <p style="text-align: justify;">We Focus on Innovative ideas, High-quality Training, Smart Classes, 100% job assistance, Opening the doors of opportunities. Our React Js Trainees are working across the nation. We at Ducat India, No#1 React Js&nbsp; Course in Noida with 100% Placement . 40+ Modules, 3 Months Course. Certified Trainers with Over 10,000 Students Trained in <strong>React Js&nbsp;Course in Noida.</strong></p>
-
-        <p style="text-align: justify;">React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called &ldquo;components&rdquo;.All these concepts and a lot more than this ,you will get to learn in React.js Training. React is a front-end library developed by Facebook. It is used for handling the view layer for web and mobile apps. ReactJS allows us to create reusable UI components. It is currently one of the most popular JavaScript libraries and has a strong foundation and large community behind it. Get certified In react.js and upgrade your skills in becoming a full stack developer .</p>
     </div>
 
 
@@ -31,13 +38,131 @@
                 <input type="text" class="form-control my-3" placeholder="Contact Number">
                 <button class=" my-3">SUBMIT</button>
             </form>
-            <textarea type="text" class="form-control" placeholder="Query Here" > -->
-</div>
-</div>
+            <textarea type="text" class="form-control" placeholder="Query Here" >
+        </div>
+    </div> -->
 
 
 
 </div>
+
+<!-- <div class="course_detail">
+    <div class="cr_detail_sec ">
+        <h4>What is software engineering?</h4>
+
+        <p class="mb-0">We Focus on Innovative ideas, High-quality Training, Smart Classes, 100% job assistance, Opening the doors of opportunities. Our React Js Trainees are working across the nation. We at Ducat India, No#1 React Js&nbsp; Course in Noida with 100% Placement . 40+ Modules, 3 Months Course. Certified Trainers with Over 10,000 Students Trained in <strong>React Js&nbsp;Course in Noida.</strong></p>
+    </div>
+</div> -->
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>How to Become a Software Engineer After 12th: Overview</h4>
+
+        <p class="mb-0">There are, mainly, two ways to gain expertise in Software Engineering.<br><br>
+
+        First and foremost choices are pursuing courses like BTech/ BSc Computer Science, BCA as
+            well as other short-term courses like Diploma in Computer Science, Diploma in Computer
+            Application and Diploma in Software Engineering, amongst others.<br><br>
+
+        The alternative way is to undertake intensive training programmes which focus on particular
+            aspects of software engineering, for instance, programming languages, design and network and
+            security, etc.<br><br>
+
+        While the former provides a more comprehensive understanding, the latter is skill-oriented and
+            emphasizes the practical aspect. Here are major things that can help you in your pursuit.</p>
+    </div>
+</div>
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>Do an Internship/Project</h4>
+
+        <p class="mb-0">During your course or training programme, grab as many opportunities of internships or project
+works as you can. These help you in gaining insights into the kinds of tasks you can expect to
+perform in the future. In addition to aiding you in finding or polishing your skills and interests, it
+gives you an idea of working in an office environment. In this guide on how to become a
+software engineer after 12th, we feel that this is an integral part of skill development.</p>
+
+       
+    </div>
+</div>
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>Complement your Skills</h4>
+
+        <p class="mb-0">The profession of software engineering and computer science involves working in diverse
+spaces. From data analysis, programming, and web designing to networking, security and
+database management and office tools, there is a lot waiting to be explored!<br><br>
+
+Entry-level opportunities require students to have the much needed zeal to learn new things and
+experiment. Apart from the aforementioned topics, Machine Learning and Data Science are also
+in store for you.</p>
+
+ </div>
+</div>
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>Certifications</h4>
+
+        <p class="mb-0">Microsoft [MTA], Microsoft Azure, Amazon Web Services Certifications, CAD, Oracle, Scrum,
+Sales force are only some of the mentions in the certifications and software courses list. These
+not only help in gaining industry-recognised knowledge and expertise but also in career
+progression and in job procurement as it stands out in your resume. The IT industry appreciates
+individuals who have attained such certifications.</p>
+
+ </div>
+</div>
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>Programming Languages Give You an Edge</h4>
+
+        <p class="mb-0">Languages of codes and programming are a world in itself. Be it Java, Basic C Programs, R,
+Python, etc, proficiency in any of them comes in handy throughout the Information Technology
+sector. In particular, such languages are used in setting up the backend operations of
+companies, application development, data analysis and much more. 
+<br><br>
+
+If you have an interest in coding, you can either go for innumerable but effective online courses
+or 8-15 weeks intensive learning programs.</p>
+
+ </div>
+</div>
+
+<div class="course_detail">
+    <div class="cr_detail_sec cr_detail_sec_out ">
+        <h4>Additional Tips</h4>
+
+        <p class="mb-0">In addition to the above, here are a few other tips on how to become a software engineer after
+12th which you must consider: 
+<br>
+
+Develop personality and career skills like teamwork, planning, organizing, problem solving,
+decision making, etc. This plays an instrumental role in career growth and progress.<br><br>
+
+Go for an advanced degree like Master of Computer Applications or MBA in Computer Science.
+As you have already raked up experience in the industry, supplementing it with such programs
+paves a way for you to enter senior level roles easily.<br><br>
+
+Improve your communication skills, both verbal and written.<br><br>
+
+Have a goal in mind with respect to the industry and the profession you wish to enter. The field
+of Computer Science Engineering is vast as it is. Develop a niche and work tirelessly towards it!<br><br>
+
+We hope this information comes in handy in understanding how to become a software engineer
+after 12th. With scores of opportunities available, it is needless to say that feeling hesitant and
+uncertain about your career is natural. If you too wish to enter this field but do not know which
+path to take, take assistance from experts at Groot Academy in a free counseling session and
+gear yourself for a vibrant career!
+
+
+</p>
+
+ </div>
+</div>
+
 
 <div class="rs-services style4 modify1 gray-color pt-40 pb-120 md-pt-75 md-pb-40 sm-pb-70">
     <div class="px-4 d-flex justify-content-around">
@@ -244,46 +369,37 @@
 
         </div>
 
-
-
-
-
-
-
-
-
-
     </div>
 </div>
-< <!-- course details ends -->
+<!-- course details ends -->
 
 
 
-    <?php include 'includes/footer.php'; ?>
+<?php include 'includes/footer.php'; ?>
 
-    <script>
-        // var acc1 = document.getElementsByClassName("contentAcc");
-        var acc = document.getElementsByTagName("h2");
-        var i;
+<script>
+    // var acc1 = document.getElementsByClassName("contentAcc");
+    var acc = document.getElementsByTagName("h2");
+    var i;
 
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
 
-                var arrow = this.firstElementChild;
-                console.log(panel);
+            var arrow = this.firstElementChild;
+            console.log(panel);
 
-                // console.log(arrow)
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                    arrow.style.transform = "rotate(180deg)";
+            // console.log(arrow)
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+                arrow.style.transform = "rotate(180deg)";
 
-                } else {
-                    panel.style.display = "block";
-                    arrow.style.transform = "rotate(0deg)";
+            } else {
+                panel.style.display = "block";
+                arrow.style.transform = "rotate(0deg)";
 
-                }
-            });
-        }
-    </script>
+            }
+        });
+    }
+</script>
